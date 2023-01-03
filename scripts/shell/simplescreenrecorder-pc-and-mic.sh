24eea8f2691707002aae9780a68843a7f100a29e
Emanuel Dramos <emanuel.dramos2@gmail.com>
2023-01-03 10:37:25 -0300

    Update

--- scripts/shell/simplescreenrecorder-pc-and-mic.sh
+++ scripts/shell/simplescreenrecorder-pc-and-mic.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+#Executar com usuario normal
 
 pactl load-module module-null-sink sink_name=duplex_out sink_properties=device.description="PC&MIC"
 pactl load-module module-loopback sink=duplex_out
