ad374f3ce9135ba2808b6d40d28b982270a04b3c
dramos <83480629+dramos777@users.noreply.github.com>
2025-05-30 08:45:12 -0300

    Update swappiness.sh

--- custom/server/scripts/swappiness.sh
+++ custom/server/scripts/swappiness.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-echo "vm.swappiness=1" > /etc/sysctl.d/swappiness.conf
+echo "vm.swappiness=10" > /etc/sysctl.d/swappiness.conf
 echo "vm.vfs_cache_pressure=50" >> /etc/sysctl.d/swappiness.conf
-echo 1 > /proc/sys/vm/swappiness
+echo 10 > /proc/sys/vm/swappiness
